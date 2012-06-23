4dede6751a62d832bd4db933adc2a43a97469d99
Jorge Barnaby <jorge.barnaby@gmail.com>
2012-06-23 10:02:26 -0400

    environments/development updated for rails 3.2.6 and omniauth 1.1.0.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,8 +2,8 @@ OmniauthDemo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the webserver when you make code changes.
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
@@ -11,7 +11,6 @@ OmniauthDemo::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -22,6 +21,19 @@ OmniauthDemo::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
   
   Rails.application.config.middleware.use OmniAuth::Builder do
     # ALWAYS RESTART YOUR SERVER IF YOU MAKE CHANGES TO THESE SETTINGS!
@@ -39,21 +51,20 @@ OmniauthDemo::Application.configure do
     provider :github, 'CLIENT ID', 'SECRET'
     
     # generic openid
-    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
+    provider :openid, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'openid'
     
     # dedicated openid
-    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-    # provider :google_apps, OpenID::Store::Filesystem.new('./tmp'), :name => 'google_apps'
+    provider :openid, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+    # provider :google_apps, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'google_apps'
     # /auth/google_apps; you can bypass the prompt for the domain with /auth/google_apps?domain=somedomain.com
     
-    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'yahoo', :identifier => 'yahoo.com' 
-    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'aol', :identifier => 'openid.aol.com'
-    provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'myopenid', :identifier => 'myopenid.com'
+    provider :openid, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com' 
+    provider :openid, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'aol', :identifier => 'openid.aol.com'
+    provider :openid, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'myopenid', :identifier => 'myopenid.com'
     
     # Sign-up urls for Facebook, Twitter, and Github
     # https://developers.facebook.com/setup
     # https://github.com/account/applications/new
     # https://developer.twitter.com/apps/new
- end
+  end
 end
-
